165e68550f80742e8743c02c291f11ed02f85297
YairGG294 <gutierrez.gonzalez.yair.norel@gmail.com>
2025-11-15 19:55:28 -0600

    Avances Tablas 4-11

--- src/main/java/com/example/demo/model/comentario/RespuestaComentario.java
+++ src/main/java/com/example/demo/model/comentario/RespuestaComentario.java
@@ -1,8 +1,32 @@
 package com.example.demo.model.comentario;
 
 import com.example.demo.model.base.Entidad;
+import com.example.demo.model.sesion.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_respuestaComentario")
 
 public class RespuestaComentario extends Entidad {
 
+    @Column(nullable = false, length = 2000)
+    private String contenido;
 
+    @Column(name = "fechaRespuesta")
+    private LocalDateTime fechaRespuesta;
+
+    @ManyToOne
+    @JoinColumn(name = "idComentario", nullable = false)
+    private Comentario comentario;
+
+    @ManyToOne
+    @JoinColumn(name = "idAdministrador", nullable = false)
+    private Usuario administrador;
 }
